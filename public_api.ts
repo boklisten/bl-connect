9224cf23bc9d9d89154fa9c388eca815de66a694
holskil <aholskil@gmail.com>
2017-11-25 12:41:39 +0100

    fix(branchService): exported branch service in public api

--- public_api.ts
+++ public_api.ts
@@ -2,4 +2,5 @@
 
 export * from "./src/app/bl-connect/bl-connect.module";
 export * from "./src/app/item/item.service";
+export * from "./src/app/branch/branch.service";
 export * from "./src/app/api/api.service";
