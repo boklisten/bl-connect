7440248530051f6bfb4b38e7c4308fd1dc03fe00
holskil <aholskil@gmail.com>
2018-06-18 14:27:55 +0200

    fix: export line in public_api for emailValidationService

--- public_api.ts
+++ public_api.ts
@@ -16,3 +16,4 @@ export * from "./src/app/payment/payment.service";
 export * from "./src/app/delivery/delivery.service";
 export * from "./src/app/branch-item/branch-item-service";
 export * from "./src/app/password-reset/password-reset.service";
+export * from "./src/app/email-validation/email-validation.service";
