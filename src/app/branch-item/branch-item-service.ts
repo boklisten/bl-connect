1281b8cf6d6fb17d9b9fff2d0ad3c02d1227d506
holskil <aholskil@gmail.com>
2018-06-30 23:38:11 +0200

    feat: branchItem service to use cachedDocumentService

--- src/app/branch-item/branch-item-service.ts
+++ src/app/branch-item/branch-item-service.ts
@@ -3,37 +3,37 @@ import {DocumentService} from "../document/document.service";
 import {BranchItem} from "@wizardcoder/bl-model";
 import {ApiService} from "../api/api.service";
 import {BL_CONFIG} from "../bl-connect/bl-config";
+import {CachedDocumentService} from "../document/cached-document.service";
 
 
 @Injectable()
 export  class BranchItemService {
 	private _collectionName: string;
 	private _documentName: string;
-	private _documentService: DocumentService<BranchItem>;
 
-	constructor(private _apiService: ApiService) {
+	constructor(private _apiService: ApiService, private _cachedDocumentService: CachedDocumentService<BranchItem>) {
 		this._collectionName = BL_CONFIG.collection.branchItem;
-		this._documentService = new DocumentService<BranchItem>(this._collectionName, this._apiService);
+		_cachedDocumentService._documentService = new DocumentService<BranchItem>(this._collectionName, this._apiService);
 	}
 
 	public get(query?: string): Promise<BranchItem[]> {
-		return this._documentService.get(query);
+		return this._cachedDocumentService.get(query);
 	}
 
 	public getById(id: string): Promise<BranchItem> {
-		return this._documentService.getById(id);
+		return this._cachedDocumentService.getById(id);
 	}
 
 	public getManyByIds(ids: string[]): Promise<BranchItem[]> {
-		return this._documentService.getManyByIds(ids);
+		return this._cachedDocumentService.getManyByIds(ids);
 	}
 
 	public update(id: string, data: any): Promise<BranchItem> {
-		return this._documentService.update(id, data);
+		return this._cachedDocumentService.update(id, data);
 	}
 
 	public add(branchItem: BranchItem): Promise<BranchItem> {
-		return this._documentService.add(branchItem);
+		return this._cachedDocumentService.add(branchItem);
 	}
 }
 
