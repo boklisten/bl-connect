d8d429642dda2ac72d4967ab3fc8638c4e996f7b
holskil <aholskil@gmail.com>
2018-06-30 23:36:45 +0200

    feat: openingHour service to use CachedDocumentService

--- src/app/opening-hour/opening-hour.service.ts
+++ src/app/opening-hour/opening-hour.service.ts
@@ -5,31 +5,31 @@ import {ApiErrorResponse} from "../api/api-error-response";
 import {ApiResponse} from "../api/api-response";
 import {BL_CONFIG} from "../bl-connect/bl-config";
 import {DocumentService} from "../document/document.service";
+import {CachedDocumentService} from "../document/cached-document.service";
 
 @Injectable()
 export class OpeningHourService {
 	private _collectionName: string;
-	private _documentService: DocumentService<OpeningHour>;
 
-	constructor(private _apiService: ApiService) {
+	constructor(private _apiService: ApiService, private _cachedDocumentService: CachedDocumentService<OpeningHour>) {
 		this._collectionName = BL_CONFIG.collection.openingHour;
-		this._documentService = new DocumentService<OpeningHour>(this._collectionName, this._apiService);
+		_cachedDocumentService._documentService = new DocumentService<OpeningHour>(this._collectionName, this._apiService);
 	}
 
 	public getById(id: string): Promise<OpeningHour> {
-		return this._documentService.getById(id);
+		return this._cachedDocumentService.getById(id);
 	}
 
 	public getManyByIds(ids: string[]): Promise<OpeningHour[]> {
-		return this._documentService.getManyByIds(ids);
+		return this._cachedDocumentService.getManyByIds(ids);
 	}
 
 	public update(id: string, data: any): Promise<OpeningHour> {
-		return this._documentService.update(id, data);
+		return this._cachedDocumentService.update(id, data);
 	}
 
 	public add(openingHour: OpeningHour): Promise<OpeningHour> {
-		return this._documentService.add(openingHour);
+		return this._cachedDocumentService.add(openingHour);
 	}
 }
 
