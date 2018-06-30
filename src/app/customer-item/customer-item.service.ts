966c2e1862faf6a6282fd81b34d8f0ba55a4da3d
holskil <aholskil@gmail.com>
2018-06-30 23:40:40 +0200

    feat: customerItem service to use cachedDocumentService

--- src/app/customer-item/customer-item.service.ts
+++ src/app/customer-item/customer-item.service.ts
@@ -3,32 +3,32 @@ import {ApiService} from "../api/api.service";
 import {BL_CONFIG} from "../bl-connect/bl-config";
 import {DocumentService} from "../document/document.service";
 import {CustomerItem} from "@wizardcoder/bl-model";
+import {CachedDocumentService} from "../document/cached-document.service";
 
 @Injectable()
 export class CustomerItemService {
 	private _collectionName: string;
-	private _documentService: DocumentService<CustomerItem>;
-	
-	constructor(private _apiService: ApiService) {
+
+	constructor(private _apiService: ApiService, private _cachedDocumentService: CachedDocumentService<CustomerItem>) {
 		this._collectionName = BL_CONFIG.collection.customerItem;
-		this._documentService = new DocumentService<CustomerItem>(this._collectionName, this._apiService);
+		_cachedDocumentService._documentService = new DocumentService<CustomerItem>(this._collectionName, this._apiService);
 	}
-	
-	
+
+
 	public add(customerItem: CustomerItem): Promise<CustomerItem> {
-		return this._documentService.add(customerItem);
+		return this._cachedDocumentService.add(customerItem);
 	}
-	
+
 	public getById(id: string): Promise<CustomerItem> {
-		return this._documentService.getById(id);
+		return this._cachedDocumentService.getById(id);
 	}
-	
+
 	public getManyByIds(ids: string[]): Promise<CustomerItem[]> {
-		return this._documentService.getManyByIds(ids);
+		return this._cachedDocumentService.getManyByIds(ids);
 	}
-	
+
 	public update(id: string, data: any): Promise<CustomerItem> {
-		return this._documentService.update(id, data);
+		return this._cachedDocumentService.update(id, data);
 	}
-	
+
 }
