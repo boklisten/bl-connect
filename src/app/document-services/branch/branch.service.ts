3741476fb025c809d85723cdc32616740e1bf59f
Lars Mitsem Selbekk <lars@selbekk.com>
2024-04-30 20:50:56 +0200

    Revert "feat(branch): add upper secondary check method"
    This reverts commit fbf473f56682697df89ffd0720fbd4c2fa695428.

--- src/app/document-services/branch/branch.service.ts
+++ src/app/document-services/branch/branch.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Branch } from "@boklisten/bl-model";
+import { ApiService } from "../../api/api.service";
 import { BL_CONFIG } from "../../bl-connect/bl-config";
+import {
+	CachedDocumentService,
+	CachedDocumentServiceOptions,
+} from "../../document/cached-document.service";
 import { BlDocumentService } from "../../document/bl-document.service";
-import { CachedDocumentService } from "../../document/cached-document.service";
 
 @Injectable()
 export class BranchService extends BlDocumentService<Branch> {
 	constructor(private cachedDocumentService: CachedDocumentService) {
 		super(cachedDocumentService);
 		this.setCollection(BL_CONFIG.collection.branch);
-	}
-
-	public isUpperSecondarySchool(branch: Branch): boolean {
-		return branch.type === "VGS";
 	}
 }
