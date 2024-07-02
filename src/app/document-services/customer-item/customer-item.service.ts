1a2cf7b7d7e29bde452d91b9b0fd4ca014939a2b
Adrian Andersen <adrianandersen@protonmail.com>
2024-07-02 23:55:42 +0200

    customer item reporting (#230)
    * feat(customer-item.service): add generate report method
    
    * 0.20.7
    
    * feat(customer-item.service): add creationTime filtering
    
    * 0.20.8
    
    * feat(customer-item-reports): remove handout argument
    
    * 0.20.9
    
    * feat(customer-item-reports): update api
    
    * 0.20.10

--- src/app/document-services/customer-item/customer-item.service.ts
+++ src/app/document-services/customer-item/customer-item.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from "@angular/core";
 import { CustomerItem } from "@boklisten/bl-model";
-import { ApiService } from "../../api/api.service";
 import { BL_CONFIG } from "../../bl-connect/bl-config";
-import {
-	CachedDocumentService,
-	CachedDocumentServiceOptions,
-} from "../../document/cached-document.service";
+import { CachedDocumentService } from "../../document/cached-document.service";
 import { BlDocumentService } from "../../document/bl-document.service";
+import { ApiService } from "../../api/api.service";
 
 @Injectable()
 export class CustomerItemService extends BlDocumentService<CustomerItem> {
-	constructor(private cachedDocumentService: CachedDocumentService) {
+	constructor(
+		private cachedDocumentService: CachedDocumentService,
+		private _apiService: ApiService
+	) {
 		super(cachedDocumentService);
 		this.setCollection(BL_CONFIG.collection.customerItem);
+	}
+
+	public async generateCustomerItemReport(options: {
+		branchFilter?: string[];
+		createdAfter?: Date;
+		createdBefore?: Date;
+		returned: boolean;
+		buyout: boolean;
+	}): Promise<unknown[]> {
+		return (
+			await this._apiService.add(
+				BL_CONFIG.collection.customerItem + "/generate-report",
+				options
+			)
+		).data;
 	}
 }
